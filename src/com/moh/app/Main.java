ca9f1e8c67a1a13bb17516c6c16c6ab45c33ca17
Moe <mallibalogun18@gmail.com>
2022-04-23 15:06:57 -0400

    added a static method to print welcome message

--- src/com/moh/app/Main.java
+++ src/com/moh/app/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World!!");
+        System.out.println("Hello World!");
+        welcomeMessage("James Brown");
+
+    }
+
+    public static void welcomeMessage(String name){
+        System.out.println("Welcome to our Java Program "+name+"!!");
     }
 }
